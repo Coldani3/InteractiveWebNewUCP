4e5e0635e8c10ab3e090cfafac90f7798dee7c3c
Coldani3 <p110117069@gmail.com>
2020-11-04 12:41:10 +0000

    Working on NewsBox

--- src/components/NewsBox.vue
+++ src/components/NewsBox.vue
@@ -1,11 +1,36 @@
 <template>
-    <div id="newsBox">
-        
+    <div id="newsBox" class="box">
+        <header class="hero teal" style="border-style:solid;border-color:black;border-width:1px;height:50px;">
+            <div class="hero-body">
+                <p class="title">{{title}}</p>
+            </div>
+        </header>
+        <article>
+            <p>{{article}}</p>
+        </article>
+        <footer>
+            <a class="button" v-bind:href="full-article-link" style="float:right;">Full article</a>
+        </footer>
     </div>
 </template>
 
 <script>
 export default {
-    
+    el: "newsBox",
+    props: {
+        "title": String,
+        "full-article-link": String
+    },
+    data()
+    {
+        return {
+            title: "",
+            article: "",
+        }
+    }
 }
 </script>
+
+<style scoped>
+
+</style>
