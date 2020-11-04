4bad7b72e0a18c5322f835cd6fc7a732dca4ec37
Coldani3 <p110117069@gmail.com>
2020-11-04 13:52:25 +0000

    More work on NewsBox.vue, just need to finish image sizing and we're done with it.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <NewsBox title="Test title" full-article-link="https://ucp.ac.uk/" article="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non venenatis libero, quis rhoncus dui. Nam ut elit at arcu sollicitudin rutrum non eget diam. Nunc pulvinar enim sed massa placerat ultrices. Cras et nisi at nunc fermentum feugiat non vel erat. Ut ultricies sem scelerisque tellus pretium sagittis. Curabitur iaculis tempor venenatis. Aliquam magna tellus, sodales et congue quis, elementum ut lorem. Donec vulputate, nunc et iaculis ornare, massa metus congue diam, vel tristique odio purus sed sapien. Nunc fermentum sapien sem, sed dapibus eros lacinia eget. Morbi aliquam eleifend tortor, eu sagittis eros aliquam vitae."/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+import NewsBox from "./components/NewsBox.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    NewsBox
   }
 };
 </script>
--- src/components/NewsBox.vue
+++ src/components/NewsBox.vue
@@ -1,15 +1,19 @@
 <template>
-    <div id="newsBox" class="box">
-        <header class="hero teal" style="border-style:solid;border-color:black;border-width:1px;height:50px;">
-            <div class="hero-body">
-                <p class="title">{{title}}</p>
-            </div>
+    <div id="newsBox" style="margin-left:10px;margin-right:10px;border-style:solid;border-color:black;border-width:1px;border-radius:5px;text-align:left;">
+        
+        <header class="teal" style="height:50px;border-top-left-radius:5px;border-top-right-radius:5px;">
+            <p class="title">{{title}}</p>
         </header>
-        <article>
-            <p>{{article}}</p>
+        <article style="padding:2px;">
+            <img style="float:left;margin-right:2px;border-style:solid;border-width:1px;border-color:black;height:150px;width:150px;" v-bind:src="imageLink" alt="image">
+            <p style="margin-bottom:2px;">{{article}}</p>
+            
+            <div align="right">
+                <a class="button" v-bind:href="fullArticleLink">Full article</a>
+            </div>
         </article>
         <footer>
-            <a class="button" v-bind:href="full-article-link" style="float:right;">Full article</a>
+            
         </footer>
     </div>
 </template>
@@ -17,15 +21,18 @@
 <script>
 export default {
     el: "newsBox",
+    name: "NewsBox",
     props: {
-        "title": String,
-        "full-article-link": String
+        title: String,
+        fullArticleLink: String,
+        article: String,
+        imageLink: String
     },
     data()
     {
         return {
-            title: "",
-            article: "",
+            //title: "",
+            //article: "",
         }
     }
 }
