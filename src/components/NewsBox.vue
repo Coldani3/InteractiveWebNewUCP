1ce21d04075536fd5febbdc2ccf906045c205325
Coldani3 <p110117069@gmail.com>
2020-11-04 14:09:07 +0000

    Set max image size for NewsBox, we're pretty much done with it now.

--- src/components/NewsBox.vue
+++ src/components/NewsBox.vue
@@ -1,13 +1,13 @@
 <template>
-    <div id="newsBox" style="margin-left:10px;margin-right:10px;border-style:solid;border-color:black;border-width:1px;border-radius:5px;text-align:left;">
+    <div id="newsBox" class="mainBox">
         
-        <header class="teal" style="height:50px;border-top-left-radius:5px;border-top-right-radius:5px;">
+        <header class="teal headerBox">
             <p class="title">{{title}}</p>
         </header>
         <article style="padding:2px;">
-            <img style="float:left;margin-right:2px;border-style:solid;border-width:1px;border-color:black;height:150px;width:150px;" v-bind:src="imageLink" alt="image">
-            <p style="margin-bottom:2px;">{{article}}</p>
-            
+            <img class="articleImage" v-bind:src="imageLink" alt="image">
+            <p class="articleBody">{{article}}</p>
+
             <div align="right">
                 <a class="button" v-bind:href="fullArticleLink">Full article</a>
             </div>
@@ -27,17 +27,42 @@ export default {
         fullArticleLink: String,
         article: String,
         imageLink: String
-    },
-    data()
-    {
-        return {
-            //title: "",
-            //article: "",
-        }
     }
 }
 </script>
 
 <style scoped>
+.articleImage
+{
+    float:left;
+    margin-right:2px;
+    border-style:solid;
+    border-width:0.5px;
+    border-color:black;
+    max-height:150px;
+    max-width:150px;
+}
 
+.articleBody
+{
+    margin-bottom:2px;
+}
+
+.mainBox
+{
+    margin-left:10px;
+    margin-right:10px;
+    border-style:solid;
+    border-color:black;
+    border-width:1px;
+    border-radius:5px;
+    text-align:left;
+}
+
+.headerBox
+{
+    height:50px;
+    border-top-left-radius:5px;
+    border-top-right-radius:5px;
+}
 </style>
