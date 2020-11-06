c49c69c112e1425f11d6e19c0679ce0922cb194e
Coldani3 <p110117069@gmail.com>
2020-11-06 01:11:06 +0000

    Begin work on vertical navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="navBar">
+        <button v-on:click="toggleDisplayed" class="dark-teal">TODO: HAMBURGER</button>
+        <div class="dark-teal mainBox" v-if="expanded">
+            <!-- TODO: Somehow get the links and display them -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    el: "#navBar",
+    props: {
+        expandedDefault: false
+    },
+    data()
+    {
+        return {
+            expanded: expandedDefault
+        }
+    },
+    methods: {
+        setDisplayed(isExpanded)
+        {
+            expanded = isExpanded;
+        },
+        toggleDisplayed()
+        {
+            expanded = !expanded;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.mainBox {
+    float: left;
+    width: 100px;
+    height: 100%;
+}
+
+.link {
+    color: lightseagreen;
+    font-size: 18px;
+    text-emphasis: bolder;
+}
+
+.link:visited {
+    color: blueviolet;
+}
+</style>
