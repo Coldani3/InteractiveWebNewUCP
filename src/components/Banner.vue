872eeed4cedc6f822cc529a81797b24436dd1fe3
Coldani3 <p110117069@gmail.com>
2020-11-11 09:57:21 +0000

    Banner UCP logo added.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,9 +1,13 @@
 <template>
     <div id="siteBanner" class="bannerDiv">
-        <img class="bannerImage" src="@/assets/logo.png" alt="logo">
+        <img class="bannerImage" src="@/assets/ucp__logo.png" alt="logo">
 
-        <div class="loginSection">
-            
+        <div class="loginSection box">
+            <div>
+                <a href=""><img class="far fa-user-circle"> Student Login</a>
+                |
+                <a href=""><img class="far fa-user-circle"> Staff Login</a>
+            </div>
         </div>
     </div>
 </template>
@@ -24,9 +28,17 @@ export default {
 .bannerDiv {
     width: 100%;
     height: 150px;
+    padding-bottom: 5px;
 }
 
 .bannerImage {
     max-height: 100%;
+}
+
+.loginSection {
+    float:right;
+    border-top-right-radius: 0px;
+    border-top-left-radius: 0px;
+    border-bottom-right-radius: 0px;
 }
 </style>
